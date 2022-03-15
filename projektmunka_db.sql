df5aa2ebadfee884b2538170f488ce92b825a46a
monaprojekt <78700178+monaprojekt@users.noreply.github.com>
2022-03-15 22:53:19 +0100

    Add files via upload
    2 ID típus módosítása.

--- projektmunka_db.sql
+++ projektmunka_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2022. Már 09. 09:42
+-- Létrehozás ideje: 2022. Már 15. 22:23
 -- Kiszolgáló verziója: 10.4.21-MariaDB
 -- PHP verzió: 8.0.11
 
@@ -34,7 +34,7 @@ CREATE TABLE `alkalmazott` (
   `alk_ID` int(255) NOT NULL COMMENT 'Alkalmazott (user) egyedi azonosítója.',
   `alk_fnev` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'Alkalmazott felhasználóneve.',
   `alk_pw` varchar(100) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'Alkalmazotti jelszó.',
-  `jog_ID` int(255) NOT NULL COMMENT 'Alkalmazotti jogok pl. admin.',
+  `jog_ID` int(255) NOT NULL COMMENT 'Alkalmazotti jogok megnevezése pl. admin.',
   `jog_s` tinyint(1) NOT NULL COMMENT 'Jog státusza (aktív, megszűnt)',
   `alk_aj` int(10) NOT NULL COMMENT 'Adóazonosító jel - alkalmazotti egyedi azonosító (humán).',
   `alk_nev0` varchar(10) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'Név előjel: Dr., PHD, PROF...',
@@ -59,11 +59,9 @@ CREATE TABLE `alkalmazott` (
 --
 
 INSERT INTO `alkalmazott` (`alk_ID`, `alk_fnev`, `alk_pw`, `jog_ID`, `jog_s`, `alk_aj`, `alk_nev0`, `alk_nev`, `alk_jv_0`, `alk_jv_1`, `alk_szhely`, `alk_szido`, `alk_mail`, `alk_tel`, `alk_beoszt`, `alk_vez`, `alk_p`, `alk_regido`, `alk_s`, `alk_ki`, `alk_mj`) VALUES
-(1, 'admin', '0cc175b9c0f1b6a831c399e269772661', 0, 0, 2147483647, 'dr.', 'Teszt Admin', NULL, NULL, 'Budapest', '1987-09-24', 'admin@teszt.com', '06201231234', 'gyakornok', 0, 0, '2022-03-06 17:42:02', 'aktív', 0, 'új belépő'),
-(8, 'user', '0cc175b9c0f1b6a831c399e269772661', 0, 0, 1123243244, '', 'Teszt User', NULL, NULL, 'Baja', '1987-12-01', 'tesztuser@teszt.hu', '06203457655', 'portás', 0, 1, '2022-03-07 08:28:46', 'aktív', 0, 'új belépő'),
-(9, 'tesztvezeto', '0cc175b9c0f1b6a831c399e269772661', 0, 0, 2147483647, '', 'Teszt Vezető', NULL, NULL, 'Miskolc', '1988-01-23', 'tesztvezeto@teszt.hu', '06303456522', 'főreferens', 1, 0, '2022-03-07 08:39:24', 'aktív', 0, 'belépett: 2022.03.07.'),
-(10, 'a', '0cc175b9c0f1b6a831c399e269772661', 0, 0, 1, 'a', 'a', '2022-03-15', '2022-03-09', 'a', '2022-03-30', 'a@g.hu', '1', 'a', 0, 0, '2022-03-07 09:20:52', 'a', 0, 'a'),
-(11, 'v', '9e3669d19b675bd57058fd4664205d2a', 0, 0, 1, 'v', 'v', '2022-03-03', '2022-03-11', 'a', '2022-04-07', 'a@g.hu', '1', 'v', 0, 0, '2022-03-07 09:25:19', '1', 0, '');
+(1, 'admin', '0cc175b9c0f1b6a831c399e269772661', 1, 0, 1234567898, 'dr.', 'Teszt Admin', '2021-08-03', '0000-00-00', 'Budapest', '1987-09-24', 'admin@teszt.com', '06201231234', 'főmunkatárs', 0, 0, '2022-03-06 17:42:02', 'aktív', 0, ''),
+(8, 'user', '0cc175b9c0f1b6a831c399e269772661', 5, 0, 1234567898, '', 'Teszt User', '2016-03-01', '0000-00-00', 'Baja', '1987-12-01', 'tesztuser@teszt.hu', '06203457655', 'portás', 0, 1, '2022-03-07 08:28:46', 'aktív', 0, ''),
+(9, 'tesztvezeto', '0cc175b9c0f1b6a831c399e269772661', 0, 0, 2121212123, '', 'Teszt Vezető', '2022-03-01', NULL, 'Miskolc', '1988-01-23', 'tesztvezeto@teszt.hu', '06303456522', 'főreferens', 1, 0, '2022-03-07 08:39:24', 'aktív', 0, '');
 
 -- --------------------------------------------------------
 
@@ -80,30 +78,6 @@ CREATE TABLE `beki` (
   `alk_ip` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'Milyen ip címről lépett be?',
   `alk_ses` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'Session - aktuális munkamenet.'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
-
---
--- A tábla adatainak kiíratása `beki`
---
-
-INSERT INTO `beki` (`beki_ID`, `alk_fnev`, `alk_be`, `alk_ki`, `alk_ip`, `alk_ses`) VALUES
-(1, '', '0000-00-00 00:00:00', '2022-03-06 17:42:39', '127.0.0.1', '2'),
-(2, 'admin', '2022-03-06 17:42:47', '2022-03-06 17:43:02', '127.0.0.1', '2'),
-(3, 'admin', '2022-03-06 17:43:10', '2022-03-06 17:57:53', '127.0.0.1', '2'),
-(4, 'admin', '2022-03-06 18:25:40', '2022-03-06 19:40:30', '127.0.0.1', '2'),
-(5, 'admin', '2022-03-07 08:18:21', '2022-03-07 08:19:17', '::1', '2'),
-(6, 'admin', '2022-03-07 08:20:18', '2022-03-07 08:26:38', '::1', '2'),
-(7, 'admin', '2022-03-07 10:29:11', '2022-03-07 11:42:03', '::1', '2'),
-(8, 'admin', '2022-03-08 21:40:07', '2022-03-08 21:45:24', '::1', ''),
-(9, 'admin', '2022-03-08 21:40:07', '2022-03-08 21:51:24', '::1', ''),
-(10, 'admin', '2022-03-08 21:57:02', '2022-03-08 21:57:05', '::1', ''),
-(11, 'admin', '2022-03-08 21:59:16', '2022-03-08 21:59:24', '::1', ''),
-(12, 'admin', '2022-03-08 22:03:03', '2022-03-08 22:03:24', '::1', ''),
-(13, 'admin', '2022-03-08 22:06:03', '2022-03-08 22:06:07', '::1', ''),
-(14, 'admin', '2022-03-08 22:08:53', '2022-03-08 22:32:54', '::1', ''),
-(15, 'admin', '2022-03-08 22:33:00', '2022-03-08 22:33:53', '::1', ''),
-(16, 'admin', '2022-03-09 08:50:20', '2022-03-09 09:01:30', '::1', ''),
-(17, 'admin', '2022-03-09 09:04:33', '2022-03-09 09:04:39', '::1', ''),
-(18, 'admin', '2022-03-09 09:11:41', '2022-03-09 09:16:21', '::1', '');
 
 -- --------------------------------------------------------
 
@@ -127,10 +101,26 @@ CREATE TABLE `gjarmu` (
 --
 
 INSERT INTO `gjarmu` (`g_ID`, `g_rsz`, `g_feng`, `g_tip`, `g_s`, `g_t`, `g_mj`) VALUES
-(1, 'q', 'q', 'q', '', 0, 'q'),
-(2, 'j', 'j', 'j', '', 0, 'j'),
-(3, 'l', 'l', 'l', '', 0, ''),
-(4, 'k', 'k', 'k', '', 0, '');
+(1, 'q', 'kmkkkk', '', '', 0, ''),
+(2, 'j', 'j', 'j', '', 1, 'j'),
+(3, 'l', 'l', 'l', '', 1, ''),
+(4, 'k', 'k', 'k', '', 1, ''),
+(5, 'MNM89898', 'mjnm 8997b 69699 ', 'jfldsajfé sjdlfjélds jfl jlfdsajf lsafas ldajflsa fa', 'állományban', 0, 'dsmnlékda f jfdélksa jflksjf lkaflkjflksj flkdsjflksajdfkd mmmmmmmmmmmmmm mmmmmmmmmmmmmmmmm mmmmmmmmm mmmmmmmmmmmmmmmmmmmmmmmmmmmmmm   mmmmmmmmmm mmmmmmmmmmm mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm mmmmmmmm mmmmmmmmmmmmmmm'),
+(6, 's', 's', 's', 's', 0, 's'),
+(7, 'ww', 'ww', 'ww', '', 0, ''),
+(8, '2', '2', '2', '2', 1, ''),
+(9, '11', '1', '1', '11', 1, ''),
+(10, 'e', 'e', 'e', '', 0, ''),
+(11, 'f', 'f', 'f', '', 0, ''),
+(12, 'x', 'x', 'x', '', 0, ''),
+(13, 'cc', 'cc', 'c', '', 0, ''),
+(14, 'y', 'y', 'y', '', 1, ''),
+(15, 'w', 'e', 'w', '', 1, ''),
+(16, '8', '8', '8', '8', 1, '8'),
+(17, '9', '9', '9', '9', 0, '9'),
+(18, 'm', 'm', 'm', '', 0, ''),
+(19, '4', '44', '4', '4', 0, '4'),
+(20, 'a', 'a', 'a', '', 0, '');
 
 -- --------------------------------------------------------
 
@@ -159,8 +149,19 @@ DROP TABLE IF EXISTS `jogok`;
 CREATE TABLE `jogok` (
   `jog_ID` int(255) NOT NULL COMMENT 'Jog azonosító.',
   `jog_nev` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'Jog megnevezése.',
+  `jog_t` tinyint(1) NOT NULL COMMENT 'Törölt jog.',
   `jog_mj` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'Joggal kapcsolatos megjegyzések.'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `jogok`
+--
+
+INSERT INTO `jogok` (`jog_ID`, `jog_nev`, `jog_t`, `jog_mj`) VALUES
+(1, 'szadmin', 1, 'Szerver admin.'),
+(2, 'kadmin', 1, 'Kliens admin.'),
+(4, 'kuser', 0, 'Kliens felhasználó.'),
+(5, 'szuser', 0, 'Szerver felhasználó.');
 
 -- --------------------------------------------------------
 
@@ -171,6 +172,7 @@ CREATE TABLE `jogok` (
 DROP TABLE IF EXISTS `kulcsfel`;
 CREATE TABLE `kulcsfel` (
   `k_fel_ID` int(255) NOT NULL COMMENT 'Kulcs felvételének azonosítója.',
+  `k_ID` int(50) NOT NULL COMMENT 'Kulcsazonosító.',
   `alk_fnev` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'Alkalmazott felhasználóneve.',
   `k_ido_0` datetime NOT NULL COMMENT 'Kulcs felvételének ideje.',
   `k_ido_1` datetime NOT NULL COMMENT 'Kulcs leadásának ideje.'
@@ -184,9 +186,10 @@ CREATE TABLE `kulcsfel` (
 
 DROP TABLE IF EXISTS `kulcsok`;
 CREATE TABLE `kulcsok` (
-  `k_ID` int(255) NOT NULL COMMENT 'Kulcsaznosító.',
-  `th_nev` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'Telephely neve.',
+  `k_ID` int(255) NOT NULL COMMENT 'Kulcsazonosító.',
+  `th_ID` int(50) NOT NULL COMMENT 'Telephely azonosítója.',
   `k_leir` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'Kulcs megnevezése, minek a kulcsa? Műhely, raktár...',
+  `k_t` tinyint(1) NOT NULL COMMENT 'Törölt kulcs.',
   `k_mj` varchar(255) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'Kulccsal kapcsolatos megjegyzések, pl. elveszett, 3 példányos...'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
@@ -215,6 +218,7 @@ CREATE TABLE `kulsos` (
 DROP TABLE IF EXISTS `telephely`;
 CREATE TABLE `telephely` (
   `th_ID` int(255) NOT NULL COMMENT 'Telephely azonosító.',
+  `th_t` int(11) NOT NULL COMMENT 'Törölt telephely.',
   `th_nev` varchar(100) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'Telephely neve.',
   `th_irsz` int(10) NOT NULL COMMENT 'Telephely irányítószáma.',
   `th_v` varchar(50) COLLATE utf8mb4_hungarian_ci NOT NULL COMMENT 'Telephely városa.',
@@ -227,16 +231,16 @@ CREATE TABLE `telephely` (
 -- A tábla adatainak kiíratása `telephely`
 --
 
-INSERT INTO `telephely` (`th_ID`, `th_nev`, `th_irsz`, `th_v`, `th_cim`, `th_gynev`, `th_mj`) VALUES
-(1, 'Népliget Center C épület', 1097, 'Budapest', 'Könyves Kálmán körút 11.', 'Központi létesítmény', ''),
-(2, 'HÉV telephely 1', 1163, 'Budapest', 'Nagyicce sor 2.', 'HÉV telephelyek', ''),
-(3, 'HÉV telephely 2', 1164, 'Budapest', 'Állomás tér 2.', 'HÉV telephelyek', ''),
-(4, 'HÉV telephely 3', 1212, 'Budapest', 'II. Rákóczi Ferenc út 174.', 'HÉV telephelyek', ''),
-(5, 'HÉV telephely 4', 1201, 'Budapest', 'Helsinki út 170196 hrsz.', 'HÉV telephelyek', ''),
-(6, 'HÉV fióktelep 1', 2000, 'Szentendre', 'Vasúti villasor 4.', 'HÉV fióktelepek', ''),
-(7, 'HÉV fióktelep 2', 2300, 'Ráckeve', 'Kossuth Lajos utca 117.', 'HÉV fióktelepek', ''),
-(8, 'HÉV fióktelep 3', 2330, 'Dunaharaszti', 'Fő út 1.', 'HÉV fióktelepek', ''),
-(9, 'HÉV fióktelep 4', 2011, 'Budakalász', 'Budakalász állomás 1236/13.', 'HÉV fióktelepek', '');
+INSERT INTO `telephely` (`th_ID`, `th_t`, `th_nev`, `th_irsz`, `th_v`, `th_cim`, `th_gynev`, `th_mj`) VALUES
+(1, 0, 'Népliget Center C épület', 1097, 'Budapest', 'Könyves Kálmán körút 11.', 'Központi létesítmény', ''),
+(2, 0, 'HÉV telephely 1', 1163, 'Budapest', 'Nagyicce sor 2.', 'HÉV telephelyek', ''),
+(3, 0, 'HÉV telephely 2', 1164, 'Budapest', 'Állomás tér 2.', 'HÉV telephelyek', ''),
+(4, 0, 'HÉV telephely 3', 1212, 'Budapest', 'II. Rákóczi Ferenc út 174.', 'HÉV telephelyek', ''),
+(5, 0, 'HÉV telephely 4', 1201, 'Budapest', 'Helsinki út 170196 hrsz.', 'HÉV telephelyek', ''),
+(6, 0, 'HÉV fióktelep 1', 2000, 'Szentendre', 'Vasúti villasor 4.', 'HÉV fióktelepek', ''),
+(7, 0, 'HÉV fióktelep 2', 2300, 'Ráckeve', 'Kossuth Lajos utca 117.', 'HÉV fióktelepek', ''),
+(8, 0, 'HÉV fióktelep 3', 2330, 'Dunaharaszti', 'Fő út 1.', 'HÉV fióktelepek', ''),
+(9, 0, 'HÉV fióktelep 4', 2011, 'Budakalász', 'Budakalász állomás 1236/13.', 'HÉV fióktelepek', '');
 
 --
 -- Indexek a kiírt táblákhoz
@@ -304,31 +308,43 @@ ALTER TABLE `telephely`
 -- AUTO_INCREMENT a táblához `alkalmazott`
 --
 ALTER TABLE `alkalmazott`
-  MODIFY `alk_ID` int(255) NOT NULL AUTO_INCREMENT COMMENT 'Alkalmazott (user) egyedi azonosítója.', AUTO_INCREMENT=12;
+  MODIFY `alk_ID` int(255) NOT NULL AUTO_INCREMENT COMMENT 'Alkalmazott (user) egyedi azonosítója.', AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT a táblához `beki`
 --
 ALTER TABLE `beki`
-  MODIFY `beki_ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `beki_ID` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT a táblához `gjarmu`
 --
 ALTER TABLE `gjarmu`
-  MODIFY `g_ID` int(255) NOT NULL AUTO_INCREMENT COMMENT 'Céges gépjármű azonosító.', AUTO_INCREMENT=5;
+  MODIFY `g_ID` int(255) NOT NULL AUTO_INCREMENT COMMENT 'Céges gépjármű azonosító.', AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT a táblához `gjarmu_km`
+--
+ALTER TABLE `gjarmu_km`
+  MODIFY `g_km_ID` int(255) NOT NULL AUTO_INCREMENT COMMENT 'Gépjármű belépési azonosító.';
 
 --
 -- AUTO_INCREMENT a táblához `jogok`
 --
 ALTER TABLE `jogok`
-  MODIFY `jog_ID` int(255) NOT NULL AUTO_INCREMENT COMMENT 'Jog azonosító.';
+  MODIFY `jog_ID` int(255) NOT NULL AUTO_INCREMENT COMMENT 'Jog azonosító.', AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT a táblához `kulcsfel`
+--
+ALTER TABLE `kulcsfel`
+  MODIFY `k_fel_ID` int(255) NOT NULL AUTO_INCREMENT COMMENT 'Kulcs felvételének azonosítója.';
 
 --
 -- AUTO_INCREMENT a táblához `kulcsok`
 --
 ALTER TABLE `kulcsok`
-  MODIFY `k_ID` int(255) NOT NULL AUTO_INCREMENT COMMENT 'Kulcsaznosító.';
+  MODIFY `k_ID` int(255) NOT NULL AUTO_INCREMENT COMMENT 'Kulcsazonosító.';
 
 --
 -- AUTO_INCREMENT a táblához `kulsos`
@@ -340,7 +356,7 @@ ALTER TABLE `kulsos`
 -- AUTO_INCREMENT a táblához `telephely`
 --
 ALTER TABLE `telephely`
-  MODIFY `th_ID` int(255) NOT NULL AUTO_INCREMENT COMMENT 'Telephely azonosító.', AUTO_INCREMENT=10;
+  MODIFY `th_ID` int(255) NOT NULL AUTO_INCREMENT COMMENT 'Telephely azonosító.', AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
