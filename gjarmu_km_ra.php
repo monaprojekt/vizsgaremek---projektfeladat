45702858dbfc11fab8516cf0143218e552bd6c94
monaprojekt <78700178+monaprojekt@users.noreply.github.com>
2022-03-16 12:14:40 +0100

    Add files via upload
    rest-api fájlok (Dani által kezelt táblákhoz)

---
+++ gjarmu_km_ra.php
@@ -0,0 +1,42 @@
+<?php
+	session_start();
+	require_once 'connect.php';
+    header('Content-Type: application/json; charset=utf-8');
+
+    if( !isset( $_GET['g_rsz'] )  ||  !isset( $_GET['g_km_0'] ) ||  !isset( $_GET['g_fel'] ) ||  !isset( $_GET['g_km_1'] ) ||  !isset( $_GET['g_le'] ) ||  !isset( $_GET['alk_fnev'] ))
+    {
+	$tomb = array( 'hiba' => "hiányos adatok" ,  'uzenet' => "megadandó paraméterek: g_rsz, g_km_0, g_fel, g_km_1, g_le, alk_fnev" ) ;
+    }
+    else
+    {
+
+		$g_rsz   = $_GET['g_rsz'] ;
+		$g_km_0   = $_GET['g_km_0'] ;
+		$g_fel   = $_GET['g_fel'] ;
+		$g_km_1   = $_GET['g_km_1'] ;
+		$g_le   = $_GET['g_le'] ;
+		$alk_fnev   = $_GET['alk_fnev'] ;
+		
+		if($db->query("
+											
+			INSERT INTO `gjarmu_km` (`g_km_ID`, `g_rsz`, `g_km_0`, `g_fel`, `g_km_1`, `g_le`, `alk_fnev`)
+			VALUES (NULL, '".$g_rsz."', '".$g_km_0."', '".$g_fel."', '".$g_km_1."', '".$g_le."', '".$alk_fnev."');
+							
+			"))
+		{
+			$tomb = array( 'hiba' => "" ,  'uzenet' => "Sikeres felvitel az adatbázisba." ) ;
+		}
+		else
+		{
+			$tomb = array( 'hiba' => "adatbázis beírás hiba" ,  'uzenet' => "Sikerestelen felvitel az adatbázisba." ) ;	
+		}
+			
+	}
+	
+	
+	
+    $json = json_encode( $tomb , JSON_UNESCAPED_UNICODE ) ;
+
+    print $json ;
+
+?>
